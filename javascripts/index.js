bdedff589b81d2c23bb6a9fde19753fc097d9417
Jonathan Blandon <jonathanblandonm6@gmail.com>
2023-03-21 12:26:27 -0600

    Box model changes

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,7 @@
 window.onload = async () => {
-  document.querySelector("#main-container").style["max-with"] = `${window.visualViewport["width"]}px`;
+  document.querySelector("#main-container").style[
+    "max-with"
+  ] = `${window.visualViewport["width"]}px`;
   console.log("loaded");
   let buttons = document.querySelectorAll(".button-link");
   var container = document.querySelector("#sub-container");
@@ -11,6 +13,33 @@ window.onload = async () => {
       let view = await (await fetch(`./partials/${target}.html`)).text();
       // history.pushState({},{},"home");
       container.innerHTML = view;
+      let parser = new DOMParser();
+      view = parser.parseFromString(view, "text/html");
+      const projectElement = view.querySelector(".project");
+      projectElement.addEventListener("click", () => {
+        console.log(1);
+        // let answer =  redirectPrompt(
+        //   "Redirecting to Play Guess Project, is ok?"
+        // );
+        // if (answer) {
+        //   window.location.href = "https://playguess.onrender.com";
+        // }
+      });
     });
   });
+
+  // const projectElement = await document.querySelectorAll(".project")
+  // console.log(projectElement);
+  // projectElement.forEach((button)=>{
+  //   console.log(3);
+  //   console.log(button.className);
+  // })
+  function redirectPrompt(message = "message") {
+    return window.confirm(message);
+  }
+  function redirect() {
+    window.alert("Jonathan");
+    console.log("object");
+    // window.location.href = "https://playguess.onrender.com";
+  }
 };
